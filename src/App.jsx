55f00194f05be3ee58d17bfb60b79fbb74d723dd
avigunjal07 <avishkargunjal07@gmail.com>
2026-04-28 12:33:23 +0530

    required components created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import CardList from './components/cardList'
+import Card from './components/card'
+
+const App = () => {
+  return (
+    <div>
+      <CardList />
+    </div>
+  )
+}
+
+export default App
