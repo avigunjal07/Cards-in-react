6a00e05e466faee061662827e02073bc538fe940
avigunjal07 <avishkargunjal07@gmail.com>
2026-04-28 12:47:59 +0530

    component Card is done with basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
-import CardList from './components/cardList'
-import Card from './components/card'
+import CardList from './components/CardList'
+import Card from './components/Card'
 
 const App = () => {
   return (
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
 const Card = () => {
+    function Card({id,title,desc,imgUrl}){
+        const cardStyle={
+            display:"flex",
+            justifyContent:"center",
+            alignItems:"center",
+        }
+        const imgStyle={
+            borderRadius:"15px",
+            height:"75px",
+            width:"75px",
+        }
+    }
   return (
-    <div>
+    <div style={cardStyle}>
+        <h3>`ID : ${id}`</h3>
+        <img src={imgUrl} alt="" style={imgStyle}/>
+        <h4>{title}</h4>
+        <p>desc</p>
       
     </div>
   )
